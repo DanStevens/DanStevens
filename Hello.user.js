3613c57aaeb095d58bcb04be8558c97896369465
Dan Stevens <dan.stevens@oracle.com>
2016-03-31 09:38:51 +0100

    Added user script for sanity testing

---
+++ Hello.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Hello, World!
+// @namespace   doomy.co.uk
+// @description Sanity test user script
+// @include     http://example.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+alert("Hello, World!");
